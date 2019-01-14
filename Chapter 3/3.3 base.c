74fa055f34f74368ab15bdafe6a5da70d2dd6e70
Tianshan Mo <cncwen@126.com>
2019-01-15 00:42:04 +0800

    Chaper 2 and Chapter 3
    Chaper 2 and Chapter 3

---
+++ Chapter 3/3.3 base.c
@@ -0,0 +1,11 @@
+/* base.c--prints 100 in decimal, octal, and hex */
+#include <stdio.h>
+int main(void)
+{
+	int x = 100;
+
+	printf("dec = %d; octal = %o; hex = %x\n", x, x, x);
+	printf("dec = %d; octal = %#o; hex = %#x\n", x, x, x);
+
+	return 0;
+}
